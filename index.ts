bd76fa4c583bc3e0474a9654682f029787b712a7
Charlie Chen <conechan@gmail.com>
2022-11-22 15:20:31 +0800

    feat: 完成功能

---
+++ index.ts
@@ -0,0 +1,73 @@
+import { downloadAsset, getLessonData, ILessonDataType, outputMd } from './utils'
+import fse from 'fs-extra'
+import path from 'path'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const form = {
+  mem_id: process.env.MEM_ID,
+  comment_reply: '-1'
+}
+
+const cookie = process.env.COOKIE ?? ''
+
+const dist = process.env.DIST ?? ''
+
+main()
+
+export async function main() {
+  if (!fse.pathExistsSync(dist)) {
+    fse.ensureDirSync(dist)
+  }
+
+  const lessons = await getLessonData(form, cookie)
+
+  const missLessons = lessons
+    //
+    .filter(lesson => {
+      // 找到不存在的日期
+      return !fse.pathExistsSync(path.join(dist, lesson.dated))
+    })
+
+  // console.log(missLessons)
+
+  for (const lesson of missLessons) {
+    // 创建日期文件夹
+    const lessonPath = path.join(dist, lesson.dated)
+    if (!fse.pathExistsSync(lessonPath)) {
+      fse.ensureDirSync(lessonPath)
+    }
+
+    const lessonData = lesson.comment_info.data
+
+    const images = lessonData
+      //
+      .filter(item => item.type === ILessonDataType.Image)
+      .map(item => item.url)
+
+    const videos = lessonData
+      //
+      .filter(item => item.type === ILessonDataType.Video)
+      .map(item => item.url)
+
+    const content = lessonData.find(item => item.type === ILessonDataType.Text)?.url ?? ''
+
+    for (const url of images) {
+      await downloadAsset(url, lessonPath)
+    }
+    for (const url of videos) {
+      await downloadAsset(url, lessonPath)
+    }
+
+    fse.outputFileSync(
+      path.join(lessonPath, 'readme.md'),
+      outputMd({
+        date: lesson.dated,
+        content,
+        images,
+        videos
+      })
+    )
+  }
+}
---
+++ utils.ts
@@ -0,0 +1,102 @@
+import { promisify } from 'node:util'
+import stream from 'node:stream'
+import got from 'got'
+import fse from 'fs-extra'
+import path from 'path'
+
+const pipeline = promisify(stream.pipeline)
+
+export enum ILessonDataType {
+  Image = '1',
+  Video = '2',
+  Text = '3'
+}
+
+export interface ILessonData {
+  url: string
+  type: ILessonDataType
+}
+
+export interface ILesson {
+  lesson_id: string
+  dated: string
+  comment_info: {
+    content: string
+    data: ILessonData[]
+  }
+}
+
+export async function getLessonData(form: any, cookie: string): Promise<ILesson[]> {
+  try {
+    // @ts-ignore
+    const { data } = await got
+      .post('https://jwb.sc-edu.com/api/wx/lesson_comment_list/', {
+        form,
+        headers: {
+          cookie
+        }
+      })
+      .json()
+    return data.lists
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
+export async function downloadAsset(url: string, dest: string) {
+  if (!fse.pathExistsSync(dest)) {
+    fse.ensureDirSync(dest)
+  }
+  const destFile = path.join(dest, path.basename(url))
+  if (!fse.pathExistsSync(destFile)) {
+    await pipeline(
+      //
+      got.stream(url),
+      fse.createWriteStream(destFile)
+    )
+    console.log(`${url} downloaded!`)
+  } else {
+    // console.log(`${url} skipped!`)
+  }
+}
+
+export function outputMd({
+  date,
+  content,
+  images,
+  videos
+}: {
+  date: string
+  content: string
+  images: string[]
+  videos: string[]
+}) {
+  let mdContent = `# ${date}\n\n`
+
+  if (content) {
+    mdContent += content.replace(/\n/g, '\n\n') + '\n\n'
+  }
+
+  if (images.length) {
+    mdContent +=
+      images
+        .map(url => {
+          const filename = path.basename(url)
+          return `![${filename}](${filename})`
+        })
+        .join('\n\n') + '\n\n'
+  }
+
+  if (videos.length) {
+    mdContent +=
+      videos
+        .map(url => {
+          const filename = path.basename(url)
+          return `<video src="${filename}" controls="controls"></video>`
+        })
+        .join('\n\n') + '\n\n'
+  }
+
+  return mdContent
+}
